7e00b7203cd0a642ca3125319733a5fba41a999f
Catherine Chernysheva <catherine.nikolaev@gmail.com>
2019-06-18 22:44:29 +0300

    add second dice

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -14,34 +14,37 @@ const RESET_VALUE = 1;
 let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
-const diceElement = document.querySelector('.dice');
+const diceElements = document.querySelectorAll('.dice');
 
 const initGame = () => {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
-  diceElement.style.display = 'none';
+  diceElements.forEach(el => el.style.display = 'none');
 }
 
 initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  let dice = Math.floor(Math.random() * 6) + 1;
+  const dices = []
 
-  diceElement.src = `dice-${dice}.png`;
-  diceElement.style.display = 'block';
+  diceElements.forEach(diceElement => {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    dices.push(dice)
+    diceElement.src = `dice-${dice}.png`;
+    diceElement.style.display = 'block';
+  })
 
-  if (dice !== RESET_VALUE) {
-    current += dice;
+  if (dices.includes(RESET_VALUE)) {
+    changePlayer();
+  } else {
+    current = dices.reduce((sum, dice) => sum + dice, current);
     document.getElementById('current-'+activePlayer).textContent = current;
 
     if (scores[activePlayer] + current >= 20) {
       alert(`Player ${activePlayer} won!!!`);
     }
-    
-  } else {
-    changePlayer();
   }
 });
 
@@ -50,7 +53,7 @@ const changePlayer = () => {
   document.getElementById('current-'+activePlayer).textContent = 0;
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
   activePlayer = +!activePlayer;
-  diceElement.style.display = 'none';
+  diceElements.forEach(el => el.style.display = 'none');
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
 }
 
